6b92f82f8281e956efa0f9c5af7094d4c8ffd8bf
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-11-10 22:53:06 +0530

    Add habit routes with create and retrieve functionality; implement database dependency

---
+++ server/app/routes/habit_routes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.core.database import SessionLocal
+from app.models.habit_model import Habit
+from app.schemas.habit_Schema import HabitCreate, HabitResponse
+from typing import List
+
+router = APIRouter()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/", response_model=HabitResponse)
+def create_habit(habit: HabitCreate, db: Session = Depends(get_db)):
+    db_habit = Habit(**habit.dict())
+    db.add(db_habit)
+    db.commit()
+    db.refresh(db_habit)
+    return db_habit
+
+
+@router.get("/", response_model=List[HabitResponse])
+def get_habits(db: Session = Depends(get_db)):
+    return db.query(Habit).all()
